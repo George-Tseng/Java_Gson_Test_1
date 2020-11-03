176017f75e79cce2c61247f8bb6dff54a7ad4196
George-Tseng <georgecycuphy@cycu.org.tw>
2020-11-03 23:34:18 +0800

    改寫ArrayList，取消泛型

--- src/gsonTwoLayerTest/GsonOutput.java
+++ src/gsonTwoLayerTest/GsonOutput.java
@@ -9,7 +9,7 @@ public class GsonOutput {
 	public static void main(String[] args) {
 		Gson gson0 = new Gson();
 		
-		List<HDD_Info> hddInfoList0 = new ArrayList<HDD_Info>();
+		List<HDD_Info> hddInfoList0 = new ArrayList<>();
 		hddInfoList0.add(new HDD_Info(1, "WD5000LPLX", "Black Label Mobile", 7200, "CMR", 32, "HDD", 2.5, "500 GB", "WD"));
 		hddInfoList0.add(new HDD_Info(2, "ST500LM034", "Barracuda Pro Mobile", 7200, "SMR", 128, "HDD", 2.5, "500 GB", "Seagate"));
 		hddInfoList0.add(new HDD_Info(3, "ST500LX025", "Firecuda Mobile", 5400, "SMR", 128, "SSHD", 2.5, "500 GB", "Seagate"));
@@ -18,7 +18,7 @@ public class GsonOutput {
 		hddInfoList0.add(new HDD_Info(6, "ST1000LX015", "Firecuda Mobile", 5400, "SMR", 128, "SSHD", 2.5, "1 TB", "Seagate"));
 		hddInfoList0.add(new HDD_Info(7, "ST2000LX001", "Firecuda Mobile", 5400, "SSHD", 128, "SSHD", 2.5, "2 TB", "Seagate"));
 		
-		List<HDD_Info> hddInfoList1 = new ArrayList<HDD_Info>();
+		List<HDD_Info> hddInfoList1 = new ArrayList<>();
 		hddInfoList1.add(new HDD_Info(101, "WD5003AZEX", "Black Label", 7200, "CMR", 64, "HDD", 3.5, "500 GB", "WD"));
 		hddInfoList1.add(new HDD_Info(102, "WD1003FZEX", "Black Label", 7200, "CMR", 64, "HDD", 3.5, "1 TB", "WD"));
 		hddInfoList1.add(new HDD_Info(103, "ST1000DX002", "Firecuda", 7200, "CMR", 64, "SSHD", 3.5, "1 TB", "Seagate"));
@@ -26,7 +26,7 @@ public class GsonOutput {
 		hddInfoList1.add(new HDD_Info(105, "ST2000DX002", "Firecuda", 7200, "CMR", 64, "SSHD", 3.5, "2 TB", "Seagate"));
 		hddInfoList1.add(new HDD_Info(106, "ST2000DM009", "Barracuda Pro", 7200, "CMR", 128, "HDD", 3.5, "2 TB", "Seagate"));
 		
-		List<HDD_Info> hddInfoList2 = new ArrayList<HDD_Info>();
+		List<HDD_Info> hddInfoList2 = new ArrayList<>();
 		hddInfoList2.add(new HDD_Info(201, "WD1005FBYZ", "Gold Label Enterprise", 7200, "CMR", 128, "HDD", 3.5, "1 TB", "WD"));
 		hddInfoList2.add(new HDD_Info(202, "ST1000NM008", "ExOS 7E2 Enterprise", 7200, "CMR", 128, "HDD", 3.5, "1 TB", "Seagate"));
 		hddInfoList2.add(new HDD_Info(203, "HUS722T1TALA604", "UltraStar DC HA210 Enterprise", 7200, "CMR", 128, "HDD", 3.5, "1 TB", "WD"));
@@ -36,11 +36,11 @@ public class GsonOutput {
 		hddInfoList2.add(new HDD_Info(207, "HUS722T2TALA604", "UltraStar DC HA210 Enterprise", 7200, "CMR", 128, "HDD", 3.5, "2 TB", "WD"));
 		hddInfoList2.add(new HDD_Info(208, "MG04ACA200E", "MG Enterprise", 7200, "CMR", 128, "HDD", 3.5, "2 TB", "Toshiba"));
 		
-		List<HDD_Info> hddInfoList3 = new ArrayList<HDD_Info>();
+		List<HDD_Info> hddInfoList3 = new ArrayList<>();
 		hddInfoList3.add(new HDD_Info(301, "WD2002FFSX", "Red Label Pro", 7200, "CMR", 64, "HDD", 3.5, "2 TB", "WD"));
 		hddInfoList3.add(new HDD_Info(302, "ST2000NE0025", "IronWolf Pro", 7200, "CMR", 128, "HDD", 3.5, "2 TB", "Seagate"));
 		
-		List<HDD> hddList0 = new ArrayList<HDD>();
+		List<HDD> hddList0 = new ArrayList<>();
 		hddList0.add(new HDD("NB", hddInfoList0));
 		hddList0.add(new HDD("Desktop", hddInfoList1));
 		hddList0.add(new HDD("DataCenter", hddInfoList2));
